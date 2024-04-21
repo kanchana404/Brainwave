f074acbafc5f2610958d6c7ddaf2f018993a0983
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-04-21 18:18:30 +0530

    Hero-fully-done

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -10,7 +10,14 @@ const Section = ({
 }) => {
   return (
    
-    <div id={id} className={`relative ${customPaddings || `py-10 lg:py-16 xl:py-20 ${crosses ? `lg:py-32 xl:py-40` : ``} ${className || ""} `}`}>
+    <div id={id}
+    className={`
+    relative 
+    ${
+      customPaddings ||
+      `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+    } 
+    ${className || ""}`}>
         {children}
         <div className='hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10'/>
         <div className='hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10'/>
