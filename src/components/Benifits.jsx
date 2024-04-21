f0326c0d5a3d8bfbb6b540e228aa37448b4b66a8
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-04-22 00:33:11 +0530

    Benifits fully done

--- src/components/Benifits.jsx
+++ src/components/Benifits.jsx
@@ -1,6 +1,6 @@
-import Section from "./Section";
-import Heading from "./Heading";
 import { benefits } from "../Constents";
+import Heading from "./Heading";
+import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
 import { GradientLight } from "./design/Benefits";
 import ClipPath from "../assets/svg/ClipPath";
@@ -17,32 +17,36 @@ const Benefits = () => {
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[245rem]"
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
               style={{
                 backgroundImage: `url(${item.backgroundUrl})`,
               }}
               key={item.id}
             >
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
-                <h5 className="h2 mb-5">{item.title}</h5>
+                <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
-                <div className="flex items-center mt-auto space">
+                <div className="flex items-center mt-auto">
                   <img
                     src={item.iconUrl}
-                    alt={item.title}
                     width={48}
                     height={48}
+                    alt={item.title}
                   />
-                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wide">Explore More</p>
-                  <Arrow/>
+                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                    Explore more
+                  </p>
+                  <Arrow />
                 </div>
               </div>
-              {item.light && <GradientLight/>}
 
-              <div  className="absolute inset-0.5 bg-n-8"
-                style={{ clipPath: "url(#benefits)" 
-              }}>
-                 <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+              {item.light && <GradientLight />}
+
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits)" }}
+              >
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
                   {item.imageUrl && (
                     <img
                       src={item.imageUrl}
@@ -54,7 +58,8 @@ const Benefits = () => {
                   )}
                 </div>
               </div>
-              <ClipPath/>
+
+              <ClipPath />
             </div>
           ))}
         </div>
